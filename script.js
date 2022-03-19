2fab0abdd902faa1c0d1ecdcce1d3957e7d6fc3b
Digdiro <ildar.grankin2@gmail.com>
2022-03-20 00:04:24 +0300

    add comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//querySelectors
 const previousOperandContainer = document.querySelector(".previous-operand");
 const currentOperationContainer = document.querySelector(".current-operation");
 const currentOperandContainer = document.querySelector(".current-operand");
@@ -7,31 +8,35 @@ const equalsButton = document.querySelector(".equals");
 const dotButton = document.querySelector(".dot");
 const deleteButton = document.querySelector(".delete");
 const allClearButton = document.querySelector(".all-clear");
-let previousOperand = "";
-let currentOperand = "";
-let currentoperation = "";
-let isNew = false;
+
+//variables
+let previousOperand = ""; //top text
+let currentOperand = ""; //bottom text
+let currentoperation = ""; //middle operation
+let isNew = false; //false all after click "equal", clear all after click numbers
 
 currentOperandContainer.textContent = currentOperand;
 previousOperandContainer.textContent = previousOperand;
 
+//operators, rounding to .000, if b = 0 display "error"
 function operate(operator, a, b = 0) {
   a = parseFloat(a);
   b = parseFloat(b);
   if (operator == "+") {
-    currentOperand = Math.floor((a + b) * 100) / 100;
+    currentOperand = Math.floor((a + b) * 1000) / 1000;
   } else if (operator == "-") {
-    currentOperand = Math.floor((a - b) * 100) / 100;
+    currentOperand = Math.floor((a - b) * 1000) / 1000;
   } else if (operator == "*") {
-    currentOperand = Math.floor(a * b * 100) / 100;
+    currentOperand = Math.floor(a * b * 1000) / 1000;
   } else if (operator == "÷") {
     if (b == 0) {
       currentOperand = "!error. Learn math.";
     } else {
-      currentOperand = Math.floor((a / b) * 100) / 100;
+      currentOperand = Math.floor((a / b) * 1000) / 1000;
     }
   }
 }
+//clear all display and all vars
 function allClear () {
   previousOperand = "";
   currentOperand = "";
@@ -40,7 +45,7 @@ function allClear () {
   previousOperandContainer.textContent = previousOperand;
   currentOperationContainer.textContent = currentoperation;
 }
-
+//operate with .value operator
 operationButtons.forEach(function (item) {
   item.addEventListener("click", function () {
     currentoperation = item.value;
@@ -50,7 +55,7 @@ operationButtons.forEach(function (item) {
     currentOperationContainer.textContent = item.value;
   });
 });
-
+//add .value number to current operator, if "isNew" is "true" clear all, and then...
 numberButtons.forEach(function (item) {
   item.addEventListener("click", function () {
     if(isNew){allClear(); isNew = false}
@@ -58,7 +63,7 @@ numberButtons.forEach(function (item) {
     currentOperandContainer.textContent = currentOperand;
   });
 });
-
+//count all and dispay result, change "previosOperator" to result expression
 equalsButton.addEventListener("click", function () {
   previousOperandContainer.textContent = 
     `${previousOperand} ${currentoperation} ${currentOperand}`;
@@ -68,15 +73,14 @@ equalsButton.addEventListener("click", function () {
   isNew = true;
 
 });
-
+//delete last char from string "currentOperator"
 deleteButton.addEventListener("click", function () {
   currentOperandContainer.textContent =
     currentOperandContainer.textContent.slice(0, -1);
     isNew = true;
 });
-
+//clear all with function
 allClearButton.addEventListener("click", allClear);
-
 dotButton.addEventListener("click", function () {
   if(currentOperand == 0){currentOperandContainer.textContent =
     currentOperandContainer.textContent.concat("0.");}
